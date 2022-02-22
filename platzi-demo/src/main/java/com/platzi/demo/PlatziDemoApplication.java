2dd0c69f456f4581430cda2b7fbdd6504e449a64
juancamilo06 <juancamilotobon06@gmail.com>
2022-02-22 14:40:47 -0500

    Hello world functional

---
+++ platzi-demo/src/main/java/com/platzi/demo/PlatziDemoApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziDemoApplication.class, args);
+	}
+
+}
---
+++ platzi-demo/src/test/java/com/platzi/demo/PlatziDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
